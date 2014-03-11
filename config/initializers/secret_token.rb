e1bdd92eaff2e9c51197054f5f36450f99865738
Malwine <malwine@mailbox.org>
2014-03-11 19:27:28 +0100

    initial commit, add all

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VeganRecipesApp::Application.config.secret_key_base = '4960818f35570100e7dc46d09a38ae26dc6884501a98ae29b7a44a12749a2c64e0bc5ab4fbf1ec3fd1b6056ab22e9011cdeb72dee1ba2ce2dcf172250cbbf9aa'
