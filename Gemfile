1d81e25a40fae7e03a5eb631420ed735dcabb87f
Malwine <malwine@mailbox.org>
2014-03-11 22:09:23 +0100

    added picture uploader and some design

--- Gemfile
+++ Gemfile
@@ -6,6 +6,9 @@ gem 'rails', '4.0.2'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 
+#Picture Upload with Carrierwave
+gem 'carrierwave'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 VeganRecipesApp::Application.routes.draw do
   resources :recipes
+  root :to => redirect('/recipes')
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
